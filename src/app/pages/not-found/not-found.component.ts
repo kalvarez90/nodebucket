7b527d80a31440cbb6d24912f84619b339bd6df3
Karina <alvarez-webdev@gmail.com>
2021-04-06 18:30:09 -0400

    last changes

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,3 +1,14 @@
+/*
+============================================
+; Title:  not-found.component.ts
+; Author: Professor Krasso
+; Date:   2 April 2021
+; Modified by: Karina Alvarez
+; Description: Not found component file
+;===========================================
+*/
+
+//These are files being imported from external files
 import { Component, OnInit } from '@angular/core';
 
 @Component({
